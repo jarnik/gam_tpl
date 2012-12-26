c17572e518d8fc436c313d729688867a306c2ce2
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2012-12-26 19:41:30 +0100

    tweening

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -15,7 +15,7 @@ import org.flixel.FlxU;
 import hsl.haxe.DirectSignaler;
 import hsl.haxe.Signaler;
 
-
+import com.eclecticdesignstudio.motion.Actuate;
 
 class PlayState extends FlxState
 {
@@ -28,6 +28,8 @@ class PlayState extends FlxState
 		
         spellCastSignaler.bind( onSpellCast );
         spellCastSignaler.dispatch( 3 );
+        
+        //Actuate.tween( this, 0.3, { scaleX: 1 } );
 	}
 
     private function onSpellCast( q:Int):Void {
