4a3484e89901d73c8c6b826ab8f37f40573aa7ba
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2012-12-26 19:36:14 +0100

    signals

--- src/GAM.hx
+++ src/GAM.hx
@@ -24,7 +24,8 @@ class GAM extends FlxGame
 		var ratioX:Float = stageWidth / 640;
 		var ratioY:Float = stageHeight / 480;
 		var ratio:Float = Math.min(ratioX, ratioY);
-		super(Math.floor(stageWidth / ratio), Math.floor(stageHeight / ratio), MenuState, ratio, 30, 30);
+		//super(Math.floor(stageWidth / ratio), Math.floor(stageHeight / ratio), MenuState, ratio, 30, 30);
+		super(Math.floor(stageWidth / ratio), Math.floor(stageHeight / ratio), PlayState, ratio, 30, 30);
 		forceDebugger = true;
 
         this.appID = appID;
---
+++ src/PlayState.hx
@@ -0,0 +1,38 @@
+package;
+
+import nme.Assets;
+import nme.geom.Rectangle;
+import nme.net.SharedObject;
+import org.flixel.FlxButton;
+import org.flixel.FlxG;
+import org.flixel.FlxPath;
+import org.flixel.FlxSave;
+import org.flixel.FlxSprite;
+import org.flixel.FlxState;
+import org.flixel.FlxText;
+import org.flixel.FlxU;
+
+import hsl.haxe.DirectSignaler;
+import hsl.haxe.Signaler;
+
+
+
+class PlayState extends FlxState
+{
+    // signals
+    public var spellCastSignaler(default, null):Signaler<Int>;
+
+	override public function create():Void
+	{
+        spellCastSignaler = new DirectSignaler(this);
+		
+        spellCastSignaler.bind( onSpellCast );
+        spellCastSignaler.dispatch( 3 );
+	}
+
+    private function onSpellCast( q:Int):Void {
+        // TODO
+        FlxG.log("magic: "+q);
+    }
+	
+}
